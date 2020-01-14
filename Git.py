76a047f8f14b541171581c10f81d5b9ce1633fa6
JoPC <hatem.saturi@gmail.com>
2020-01-14 22:22:45 +0100

    commandes git

--- Git.py
+++ Git.py
@@ -1,1 +1,6 @@
-print("f")
+PS C:\Users\Jojolelapin\Desktop\Git> git remote add origin https://github.com/Jojolelapin/GitCode.git
+PS C:\Users\Jojolelapin\Desktop\Git> git status
+PS C:\Users\Jojolelapin\Desktop\Git> git add .
+PS C:\Users\Jojolelapin\Desktop\Git> git commit -am "J'apprend git"
+PS C:\Users\Jojolelapin\Desktop\Git> git push origin master
+
